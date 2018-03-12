d7664560264e65ca7ab6cf68781b013653a30fad
jamie pittak and panatda inthala <jamie@>
2018-03-12 14:41:37 -0700

    add tests and debugged them

---
+++ spec/ping-pong-spec.js
@@ -0,0 +1,25 @@
+import {pingPong} from './../src/ping-pong.js';
+
+describe('ping', function() {
+
+  it('should test that all multiples of 3 return ping', function() {
+    var output = new pingPong(3)
+    expect(output).toEqual([1,2,'ping'])
+  });
+});
+
+describe('pong', function() {
+
+  it('should test that all multiples of 5 return pong', function() {
+    var output = new pingPong(5)
+    expect(output).toEqual([1,2,'ping',4,"pong"])
+  });
+});
+
+describe('ping-pong', function() {
+
+  it('should test that all multiples of 5 return pong', function() {
+    var output = new pingPong(15)
+    expect(output).toEqual([1,2,'ping',4,"pong","ping", 7,8,"ping","pong",11,"ping",13,14,"ping-pong"])
+  });
+});
